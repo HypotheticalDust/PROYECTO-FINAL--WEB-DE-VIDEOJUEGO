17fa0f15e8589333433b02d95f7d724a778586ae
HypotheticalDust <irene.rome346@gmail.com>
2026-04-25 00:47:24 +0200

    fix checklist next time

--- js/script.js
+++ js/script.js
@@ -31,3 +31,16 @@ function showSlides(n) {
 
 // CHECKLIST
 
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener("click", toggleDone);
+});
