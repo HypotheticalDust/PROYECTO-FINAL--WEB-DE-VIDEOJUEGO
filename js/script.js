bd269fb260555cc67a87b326fd163e8bfa44c3ef
HypotheticalDust <irene.rome346@gmail.com>
2026-04-28 00:06:57 +0200

    fixed issues

--- js/script.js
+++ js/script.js
@@ -29,18 +29,3 @@ function showSlides(n) {
   captionText.innerHTML = dots[slideIndex-1].alt;
 } 
 
-// CHECKLIST
-
-const listItems = document.querySelectorAll("li");
-
-function toggleDone(e) {
-  if (!e.target.className) {
-    e.target.className = "done";
-  } else {
-    e.target.className = "";
-  }
-}
-
-listItems.forEach((item) => {
-  item.addEventListener("click", toggleDone);
-});
